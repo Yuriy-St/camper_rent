5baac38bfdf1c4b5dccbd91ac7d0ce7cad49006f
Yuriy-St <yuriystaynov@gmail.com>
2024-04-14 22:47:31 +0300

    style/add hexToRGBA function

--- src/styles/theme/palette.js
+++ src/styles/theme/palette.js
@@ -1,35 +1,40 @@
+import { hexToRGBA } from "../../utilities";
+
 export const color = {
-  white: '255, 255, 255',
-  black: '0, 0, 0',
+  white: '#ffffff',
+  black: '#000000',
   red: {
-    300: '228, 72, 72',
-    400: '216, 67, 67',
-    500: '209, 40, 40',
+    300: '#E44848',
+    400: '#D84343',
+    500: '#D12828',
   },
   grey: {
-    100: '242, 244, 247',
-    200: '217, 217, 217',
-    600: '71, 84, 103',
-    900: '16, 24, 40',
+    100: '#F2F4F7',
+    200: '#D9D9D9',
+    600: '#475467',
+    900: '#101828',
   },
+  amber: {
+    400: '#FFC531'
+  }
 };
 
 export default {
-  main: `rgb(${color.grey[900]})`,
-  inputs: `rgb(${color.grey[100]})`,
-  blockFeatures: `rgb(${color.grey[200]})`,
-  rating: 'rgb(255, 197, 49)',
-  text: `rgb(${color.grey[600]})`,
+  main: color.grey[900],
+  inputs: color.grey[100],
+  blockFeatures: color.grey[200],
+  rating: color.amber[400],
+  text: color.grey[600],
   button: {
-    surface: `rgb(${color.red[300]})`,
-    hoverSurface: `rgb(${color.red[400]})`
+    surface: color.red[300],
+    hoverSurface: color.red[400]
   },
   buttonGhost: {
-    stroke: `rgba(${color.grey[600]}, 0.2)`,
-    hoverStroke: `rgb(${color.red[300]})`
+    stroke: hexToRGBA(color.grey[600], 20),
+    hoverStroke: color.red[300]
   },
   error: {
-    surface: `rgb(${color.red[400]})`,
-    onSurface: `rgba(${color.black}, 0.8)`,
+    surface: color.red[400],
+    stroke: hexToRGBA(color.black, 80),
   },
 };
