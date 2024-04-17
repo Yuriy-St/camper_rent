5f3cc13e32da7fefee5c444556e7e9cfe3cc88c3
Yuriy-St <45993587+Yuriy-St@users.noreply.github.com>
2024-04-17 10:09:12 +0000

    Deploying to gh-pages from @ Yuriy-St/camper_rent@14f828532b37e86ebe10849021b731a2c777a59b 🚀

---
+++ assets/Catalog-353f392c.js
@@ -0,0 +1,1 @@
+import{j as t,C as a}from"./index-8a49d28c.js";const o=()=>t.jsx(a,{children:t.jsx("p",{children:"Catalog"})});export{o as default};
---
+++ assets/Homepage-46bf81a8.js
@@ -0,0 +1,1 @@
+import{j as e,C as r}from"./index-8a49d28c.js";const s=()=>e.jsx(r,{children:e.jsx("p",{children:"Homepage"})});export{s as default};
