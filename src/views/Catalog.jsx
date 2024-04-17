eccf63c82740ad4ff228b413b6dca80751045090
Yuriy-St <yuriystaynov@gmail.com>
2024-04-17 14:27:17 +0300

    feat/create camper list component

--- src/views/Catalog.jsx
+++ src/views/Catalog.jsx
@@ -1,9 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { Container } from '../components/Container';
+import { useEffect } from 'react';
+import { fetchAllCampers } from '../redux/vehicle/operations';
+import { CamperList } from '../components/CamperList/CamperList';
 
 const Catalog = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllCampers({ page: 1, limit: 15 }));
+  }, [dispatch]);
+
   return (
     <Container>
-      <p>Catalog</p>
+      <CamperList />
     </Container>
   );
 };
