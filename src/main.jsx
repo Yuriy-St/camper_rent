a892ddca838599f64ecb2170f32b8589f345a073
Yuriy-St <yuriystaynov@gmail.com>
2024-04-25 19:16:16 +0300

    feat/add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,9 +13,11 @@ import App from './app/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter basename="/camper_rent/">
-      <Global styles={globalStyles} />
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/camper_rent/">
+        <Global styles={globalStyles} />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
